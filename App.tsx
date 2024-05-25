35c37cb5bdce70affbc0bbb3ac548381c095c6fa
Edward O'Connor <edwardoc@buffalo.edu>
2024-05-25 19:29:30 -0400

    First commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [location, setLocation] = useState<Location.LocationObject | null>(null);
+  const [transporationMethod, setTransportationMethod] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  /* Asks for users location */
+  const getDeviceLocation = async () => {
+    let {status} = await Location.requestForegroundPermissionsAsync();
+
+    if (status !== 'granted') {
+      setLoading(false);
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    setLocation(location);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getDeviceLocation();
+  }, []);
+
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {
+        /* If user allows location */
+      !loading && location != null && (
+        <View>
+          <Text>Location has been found</Text>
+          <StatusBar style="auto" />
+        </View>
+      )
+      }
+
+
+      {
+        /* if waiting for user to allow location */
+      loading && location == null && (
+        <View>
+          <Text>Waiting for Location.....</Text>
+        </View>
+      )
+      }
+
+
+      {
+        /* if waiting for user to allow location */
+      !loading && !location == null && (
+        <View>
+          <Text>Sorry you must allow location</Text>
+        </View>
+      )
+      }
+
+
+
+
+
+
+
+
+
+
     </View>
   );
 }
---
+++ test.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+
+export default function Test(){
+
+
+
+    return(
+
+        <View>
+            <Text>Test</Text>
+        </View>
+
+
+    ) 
+}
